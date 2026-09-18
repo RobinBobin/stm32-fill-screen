c3465e6f71a6561bc052252d588a1cba712d0f36
Ruben Shalimov <r_shalimov@inbox.ru>
2026-09-18 23:21:58 +0300

    feat: make leds blink

--- CM7/Src/main.c
+++ CM7/Src/main.c
@@ -21,6 +21,7 @@
 #include "dsihost.h"
 #include "gpio.h"
 #include "ltdc.h"
+#include "stm32h7xx_hal_gpio.h"
 #include "usart.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -159,6 +160,17 @@ int main(void) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+    HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+    HAL_Delay(100);
+
+    HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+    HAL_Delay(100);
+
+    HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
+    HAL_Delay(100);
+
+    HAL_GPIO_TogglePin(LED4_GPIO_Port, LED4_Pin);
+    HAL_Delay(100);
   }
   /* USER CODE END 3 */
 }
