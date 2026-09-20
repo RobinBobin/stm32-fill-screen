fdc224b541b33e3a3e6fde1afb0704b6e1fc5d9b
Ruben Shalimov <r_shalimov@inbox.ru>
2026-09-20 12:30:38 +0300

    feat: change led blinking

--- CM7/Src/toggle_leds.c
+++ CM7/Src/toggle_leds.c
@@ -1,17 +1,20 @@
 #include "toggle_leds.h"
 #include "gpio.h"
 #include "stm32h7xx_hal_gpio.h"
+#include <stdint.h>
 
 void ToggleLeds(void) {
+  const uint16_t delay = 250;
+
+  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
   HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
-  HAL_Delay(100);
+  HAL_Delay(delay);
+
+  HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+  HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+  HAL_Delay(delay);
 
   HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
-  HAL_Delay(100);
-
   HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
-  HAL_Delay(100);
-
-  HAL_GPIO_TogglePin(LED4_GPIO_Port, LED4_Pin);
-  HAL_Delay(100);
+  HAL_Delay(delay);
 }
